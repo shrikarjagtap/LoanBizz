bfe1145ddd2438f70df0aa523a458ee1fa192604
Shrikar Jagtap <119429380+shrikarjagtap@users.noreply.github.com>
2025-10-25 01:11:26 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://loanbizz-server.onrender.com' // local default; will override on Vercel with ENV var
+  apiUrl: 'https://loanbizz-server.onrender.com/api' // local default; will override on Vercel with ENV var
 };
