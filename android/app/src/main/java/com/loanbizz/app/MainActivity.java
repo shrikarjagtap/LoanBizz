21a6a411ead4c8e4eaecccb4393a46df5d1520a2
Shrikar Jagtap <shrikarjagtap341@gmail.com>
2025-12-06 20:06:01 +0530

    LoanBizz APK

---
+++ android/app/src/main/java/com/loanbizz/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.loanbizz.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.loanbizz.app',
+  appName: 'LoanBizz',
+  webDir: 'dist/loanbizz-v18-android/browser'
+};
+
+export default config;
